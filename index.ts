323ef2c25920ead0784687c5ade488fe5dcdcd3d
PJTSearch <30200738+pjtsearch@users.noreply.github.com>
2022-02-09 10:58:26 -0800

    feat: Start adding unused user adder

--- index.ts
+++ index.ts
@@ -27,6 +27,9 @@ const isUnwanted = (byPrefs: Prefs) => (user: UserId) => byPrefs.unwanted.includ
 const isWanted = (byPrefs: Prefs) => (user: UserId) => byPrefs.wanted.includes(user)
 const isGender = (gender: Gender, data: Record<string, Prefs>) => (user: UserId) => data[user].gender == gender
 
+const getUnusedUsers = (groups: Record<GroupId, Group>, data: Record<UserId, Prefs>) =>
+  Object.keys(data).filter((user) => !Object.values(groups).some((group) => group.includes(user)))
+
 export const getWantedAmount = (groups: Record<string, Group>, data: Record<string, Prefs>) =>
   Object.values(groups).flatMap((group) => group.filter((user) => group.some(isWanted(data[user])))).length
 
@@ -46,13 +49,14 @@ const getGroupScore = (group: Group, member: UserId, data: Record<string, Prefs>
 const balanceGender = (group: Group, gender: Gender, data: Record<string, Prefs>): Group => {
   let curr = group
   while (curr.filter(isGender(gender, data)).length > Math.floor(GROUP_SIZE / 2)) {
-    const leastWanted = curr.filter(isGender(gender, data)).sort((member, otherMember) => getGroupScore(group, member, data) - getGroupScore(group, otherMember, data))
+    const leastWanted = curr
+      .filter(isGender(gender, data))
+      .sort((member, otherMember) => getGroupScore(group, member, data) - getGroupScore(group, otherMember, data))
     curr = curr.filter((u) => u !== leastWanted[0])
   }
   return curr
 }
 
-// FIXME: Only do if has a greater score than someone else
 const groupWantsUser = (newUser: UserId, group: Group, data: Record<string, Prefs>): Group | undefined =>
   group
     .map((member) => {
@@ -82,35 +86,109 @@ const sortGroupsByPreference = (prefs: Prefs, groups: Record<GroupId, Group>) =>
     .map(([id, _data]) => id)
 
 const getGroups = (initial: Record<GroupId, Group>, data: Record<UserId, Prefs>) =>
-  shuffle(Object.entries(data)).reduce((groups, [id, prefs]) => {
-    // console.log({
-    //   id,
-    //   pr: sortGroupsByPreference(prefs, groups),
-    //   groups,
-    //   u: compareGroupsByPreference(prefs, groups["1"], groups["2"]),
-    // }),
+  withUnusedUsers(
+    Object.entries(data).reduce((groups, [id, prefs]) => {
+      // console.log({
+      //   id,
+      //   pr: sortGroupsByPreference(prefs, groups),
+      //   groups,
+      //   u: compareGroupsByPreference(prefs, groups["1"], groups["2"]),
+      // }),
 
-    // const inGroup = Object.entries(groups).find(([_groupId, group]) => group.includes(id))?.[0]
-    // const init = inGroup ? { ...groups, [inGroup]: groups[inGroup].filter((member) => member != id) } : groups
-    const init = groups
-    return sortGroupsByPreference(prefs, init).reduce((groups, groupId) => {
-      if (Object.values(groups).some((group) => group.includes(id))) {
+      // const inGroup = Object.entries(groups).find(([_groupId, group]) => group.includes(id))?.[0]
+      // const init = inGroup ? { ...groups, [inGroup]: groups[inGroup].filter((member) => member != id) } : groups
+      const init = groups
+      return sortGroupsByPreference(prefs, init).reduce((groups, groupId) => {
+        if (Object.values(groups).some((group) => group.includes(id))) {
+          return groups
+        } else if (
+          groups[groupId].length < GROUP_SIZE &&
+          groups[groupId].every((member) => !data[member].unwanted.includes(id))
+        ) {
+          return { ...groups, [groupId]: balanceGender([...groups[groupId], id], data[id].gender, data) }
+        } else if (groupWantsUser(id, groups[groupId], data)) {
+          return {
+            ...groups,
+            [groupId]: balanceGender(groupWantsUser(id, groups[groupId], data)!, data[id].gender, data),
+          }
+        } else {
+          return groups
+        }
+      }, init)
+    }, initial),
+    data
+  )
+
+const withUnusedUsers = (initial: Record<GroupId, Group>, data: Record<UserId, Prefs>): Record<GroupId, Group> => {
+  const groupsWithoutConflict = getUnusedUsers(initial, data).reduce((groups, userId) => {
+    const sortedGroups = Object.entries(groups)
+      .sort(([_id, group], [_otherId, otherGroup]) => group.length - otherGroup.length)
+      .filter(([_id, g]) => g.length > 0)
+      .map(([id]) => id)
+    return sortedGroups.reduce((groups, groupId) => {
+      if (Object.values(groups).flat().includes(userId)) {
         return groups
       } else if (
         groups[groupId].length < GROUP_SIZE &&
-        groups[groupId].every((member) => !data[member].unwanted.includes(id))
+        groups[groupId].every((member) => !data[member].unwanted.includes(userId)) &&
+        groups[groupId].every((member) => !data[userId].unwanted.includes(member)) &&
+        groups[groupId].filter(isGender(data[userId].gender, data)).length < Math.floor(GROUP_SIZE / 2)
       ) {
-        return { ...groups, [groupId]: balanceGender([...groups[groupId], id], data[id].gender, data) }
-      } else if (groupWantsUser(id, groups[groupId], data)) {
-        return {
-          ...groups,
-          [groupId]: balanceGender(groupWantsUser(id, groups[groupId], data)!, data[id].gender, data),
-        }
+        return { ...groups, [groupId]: [...groups[groupId], userId] }
       } else {
         return groups
       }
-    }, init)
+    }, groups)
   }, initial)
+  const groupsWithGenderConflict = getUnusedUsers(groupsWithoutConflict, data).reduce((groups, userId) => {
+    const sortedGroups = Object.entries(groups)
+      .sort(([_id, group], [_otherId, otherGroup]) => group.length - otherGroup.length)
+      .filter(([_id, g]) => g.length > 0)
+      .map(([id]) => id)
+    return sortedGroups.reduce((groups, groupId) => {
+      if (Object.values(groups).flat().includes(userId)) {
+        return groups
+      } else if (
+        groups[groupId].length < GROUP_SIZE &&
+        groups[groupId].every((member) => !data[member].unwanted.includes(userId)) &&
+        groups[groupId].every((member) => !data[userId].unwanted.includes(member))
+      ) {
+        return { ...groups, [groupId]: [...groups[groupId], userId] }
+      } else {
+        return groups
+      }
+    }, groups)
+  }, groupsWithoutConflict)
+  const groupsWithLengthConflict = getUnusedUsers(groupsWithoutConflict, data).reduce((groups, userId) => {
+    const sortedGroups = Object.entries(groups)
+      .sort(([_id, group], [_otherId, otherGroup]) => group.length - otherGroup.length)
+      .filter(([_id, g]) => g.length > 0)
+      .map(([id]) => id)
+    return sortedGroups.reduce((groups, groupId) => {
+      if (Object.values(groups).flat().includes(userId)) {
+        return groups
+      } else if (
+        groups[groupId].every((member) => !data[member].unwanted.includes(userId)) &&
+        groups[groupId].every((member) => !data[userId].unwanted.includes(member))
+      ) {
+        return { ...groups, [groupId]: [...groups[groupId], userId] }
+      } else {
+        return groups
+      }
+    }, groups)
+  }, groupsWithoutConflict)
+  console.log({
+    initial,
+    groupsWithoutConflict,
+    groupsWithGenderConflict,
+    groupsWithLengthConflict,
+    sortedGroups: Object.entries(initial)
+      .sort(([_id, group], [_otherId, otherGroup]) => group.length - otherGroup.length)
+      .filter(([_id, g]) => g.length > 0)
+      .map(([id]) => id),
+  })
+  return groupsWithLengthConflict
+}
 
 export const getGroupsIterations = (
   iterations: number,
